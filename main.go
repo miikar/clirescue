2d0a223df1f3dd400e96e7867448aead336c883a
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2016-08-02 17:10:04 +0300

    Required changes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/GoBootcamp/clirescue/trackerapi"
+	"github.com/miikar/clirescue/trackerapi"
 	"github.com/codegangsta/cli"
 )
 
--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/miikar/clirescue/cmdutil"
+	"github.com/miikar/clirescue/user"
 )
 
 var (
